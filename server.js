40916a4ff0741e5967f6e4bd7e39a9dfc25a3f2b
parvezaman <emailtoamanullah@gmail.com>
2022-09-19 23:33:02 +0600

    3/12 company definitions

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const expressGraphQL = require('express-graphql').graphqlHTTP;
+const schema = require('./schema/schema');
 
 const app = express();
 
 app.use('/graphql', expressGraphQL({
-    graphiql: true 
+    schema,
+    graphiql: true
 }));
 
-const port = 5000;
+const port = 1234;
 
 app.listen(port, () => {
     console.log(`my graphlQL learing is running on ${port} `);
