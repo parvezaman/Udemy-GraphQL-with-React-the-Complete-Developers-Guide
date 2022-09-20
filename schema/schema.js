452fa451acc6d82b7fa3f010a96820940b7c5898
parvezaman <emailtoamanullah@gmail.com>
2022-09-20 10:45:03 +0600

    4/1 company type def

--- schema/schema.js
+++ schema/schema.js
@@ -2,12 +2,22 @@ const graphql = require('graphql');
 const axios = require('axios');
 const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = graphql;
 
+const CompanyType = new GraphQLObjectType({
+    name: "Company",
+    fields: {
+        id: { type: GraphQLString },
+        name: { type: GraphQLString },
+        description: { type: GraphQLString }
+    }
+})
+
 const UserType = new GraphQLObjectType({
     name: 'User',
     fields: {
         id: { type: GraphQLString },
         firstName: { type: GraphQLString },
-        age: { type: GraphQLInt }
+        age: { type: GraphQLInt },
+        company: { type: CompanyType }
     }
 });
 
@@ -20,7 +30,7 @@ const RootQuery = new GraphQLObjectType({
             resolve(parentValue, args) {
                 // return _.find(users, { id: args.id })
                 return axios.get(`http://localhost:3000/users/${args.id}`)
-                .then(res => res.data)
+                    .then(res => res.data)
             }
         }
     }
